87368a171bb8c709c44b3bb2773776a2bc75b36b
joaoccaldas <joaoccaldas@gmail.com>
2025-12-01 19:41:17 +0100

    Initialize Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+export default {
+  content: ['./index.html', './index.tsx', './App.tsx', './components/**/*.{ts,tsx}', './services/**/*.{ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
